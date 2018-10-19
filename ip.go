a4afd4c9563f6e47a69e4cae3662f50c9f120ff7
dev <dev@dev>
2018-10-19 14:48:03 -0400

    Added method CheckIPLocalCIDR

--- ip.go
+++ ip.go
@@ -1,1 +1,64 @@
+/*
+ * Copyright (c) 2017 AlexRuzin (stan.ruzin@gmail.com)
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
 package util
+
+import (
+	"net"
+	"strings"
+)
+
+var localSubnets = []string{
+	"127.0.0.0/8",    // IPv4 loopback
+	"10.0.0.0/8",     // RFC1918
+	"172.16.0.0/12",  // RFC1918
+	"192.168.0.0/16", // RFC1918
+}
+
+func CheckIPLocalCIDR(ipAddress string) (bool, error) {
+	var addr = ipAddress
+	if !strings.Contains(ipAddress, "/") {
+		addr += "/32"
+	}
+
+	ip, _, err := net.ParseCIDR(addr)
+	if err != nil {
+		return false, err
+	}
+
+	var privateIPBlocks []*net.IPNet
+	for _, cidr := range localSubnets {
+		_, block, err := net.ParseCIDR(cidr)
+		if err != nil {
+			return false, err
+		}
+		privateIPBlocks = append(privateIPBlocks, block)
+	}
+
+	for _, block := range privateIPBlocks {
+		if block.Contains(ip) {
+			return true, nil
+		}
+	}
+
+	return false, nil
+}
--- ip_test.go
+++ ip_test.go
@@ -1,1 +1,43 @@
+/*
+ * Copyright (c) 2017 AlexRuzin (stan.ruzin@gmail.com)
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
 package util
+
+import (
+	"strconv"
+	"testing"
+)
+
+func TestCheckIPLocalCIDR(t *testing.T) {
+	var ipList = []string{ "172.16.0.2", "192.168.0.5", "192.169.0.1", "10.0.0.1", "8.8.8.8" }
+	for _, v := range ipList {
+		var ip bool
+		var err error
+		if ip, err = CheckIPLocalCIDR(v); err != nil {
+			t.Errorf(err.Error())
+			t.FailNow()
+		}
+		t.Logf("IP Address " + v + " belongs to local subnets: " + strconv.FormatBool(ip))
+	}
+
+	return
+}
